84220e435c2021829fa770ffcbe04deab4cce1e1
Lauren Spring <Lauren.Spring@seetickets.com>
2025-06-21 12:28:32 +0100

    Corrected pasting error in schema file and renamed

--- MeteringApi/Database/schema-setup.sql
+++ MeteringApi/Database/schema-setup.sql
@@ -1,18 +1,14 @@
-﻿-- Add schema changes here that are required during development.
-
-CREATE TABLE metering.account (
-	id integer NOT NULL,
+﻿CREATE TABLE metering.account (
+	id serial NOT NULL,
 	first_name varchar NOT NULL,
 	last_name varchar NOT NULL,
 	CONSTRAINT account_pk PRIMARY KEY (id)
 );
 
-
 CREATE TABLE metering.meter_reading (
-	id integer NOT NULL,
 	account_id integer NOT NULL,
-	reading_datetime date NOT NULL,
+	reading_datetime timestamp NOT NULL,
 	read_value integer NOT NULL,
-	CONSTRAINT meter_reading_pk PRIMARY KEY (id),
+	CONSTRAINT meter_reading_pk PRIMARY KEY (account_id,reading_datetime),
 	CONSTRAINT meter_reading_account_fk FOREIGN KEY (account_id) REFERENCES metering.account(id)
 );
